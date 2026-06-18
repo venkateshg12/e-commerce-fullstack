7fbffd5d3e4b7124addc51e47525ce7ae0cb9107
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-18 22:49:29 +0530

    Add auth routes

---
+++ apps/backends/mongo/src/routes/auth.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { registerHandler } from "../controllers/auth.controller";
+
+export const authRoutes = Router();
+
+// prefix : /auth
+
+authRoutes.post("/register", registerHandler)
