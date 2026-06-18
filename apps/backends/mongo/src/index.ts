903ae23f54fba7bdd2c3873bf596b53ae2409a7a
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-18 22:51:58 +0530

    commit

---
+++ apps/backends/mongo/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import "dotenv/config";
+import { CORS_ORIGIN, PORT } from "./constants/env";
+import cors from 'cors';
+import morgan from "morgan";
+import { ok } from "./utils/apiEnvelope";
+import { notFound } from "./middleware/notFound";
+import { errorHandler } from "./middleware/errorHandler";
+import { connectDB } from "./config/db";
+
+import { authRoutes } from "./routes/auth.route";
+import cookieParser from "cookie-parser";
+
+
+
+async function main() {
+    await connectDB();
+    const app = express();
+
+    const corsOrigin = (CORS_ORIGIN).split(",").map(origin => origin.trim()).filter(Boolean);
+
+    app.use(cors({ origin: corsOrigin, credentials: true }));
+    app.use(express.json());
+    app.use(morgan('dev'));
+    app.use(cookieParser());
+    
+    
+    // Routes declaration.
+    app.use("/auth", authRoutes);
+    
+    app.get("/health", async (req, res) => {
+        res.status(200).json(ok({ message: "Server is running" }));
+    })
+    
+    
+    app.listen(PORT, () => { console.log(`Server is listening to the port: ${PORT}`) })
+    
+    // Error Handlers
+    app.use(notFound);
+    app.use(errorHandler);
+}
+
+main().catch((error) => {
+    console.error("failed to start", error);
+    process.exit(1);
+})
