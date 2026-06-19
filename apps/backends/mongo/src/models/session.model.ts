303f45903f63414c58445b8071232553dae09c03
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-19 22:39:56 +0530

    Implement session model and it's types

---
+++ apps/backends/mongo/src/models/session.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { SessionDocument } from "../types/session.types";
+import { thirtyDaysFromNow } from "../utils/date";
+
+const sessionSchema = new mongoose.Schema<SessionDocument>({
+    userId : {
+        ref : "User",
+        type : mongoose.Schema.Types.ObjectId,
+        index : true,
+    },
+    userAgent : {
+        type : String,
+    },
+    createdAt : {
+        type : Date,
+        required : true,
+        default : Date.now
+    },
+    expiresAt : {
+        type : Date,
+        default :  thirtyDaysFromNow()
+    }
+})
+
+const sessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
+export default sessionModel;
