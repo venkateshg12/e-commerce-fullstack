b68e5f172e34083c58426d0ddb3c0db463e554a9
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-20 22:33:43 +0530

    Implement setAuthCookies and clearAuthCookies functionalities

--- apps/backends/mongo/src/utils/cookies.ts
+++ apps/backends/mongo/src/utils/cookies.ts
@@ -1,8 +1,10 @@
 import { CookieParams } from "@repo/types"
 import { CookieOptions } from "express";
 import { fifteenMinutesFromNow, thirtyDaysFromNow } from "./date";
+import { Response } from "express";
 
 const secure = process.env.NODE_ENV !== "development";
+export const REFRESH_PATH = "/auth/refresh"
 
 const defaults: CookieOptions = {
     sameSite: "strict",
@@ -10,16 +12,19 @@ const defaults: CookieOptions = {
     secure
 }
 
-const getAccessTokenCookieOptions = ():CookieOptions => ({
+export const getAccessTokenCookieOptions = (): CookieOptions => ({
     ...defaults,
-    expires : fifteenMinutesFromNow()
+    expires: fifteenMinutesFromNow()
 })
 
-const getRefreshTokenCookieOptions = ():CookieOptions => ({
+export const getRefreshTokenCookieOptions = (): CookieOptions => ({
     ...defaults,
-    expires : thirtyDaysFromNow(),
-    path : "/auth/refresh"
+    expires: thirtyDaysFromNow(),
+    path: REFRESH_PATH
 })
 
 export const setAuthCookies = ({ res, accessToken, refreshToken }: CookieParams) =>
     res.cookie("accessToken", accessToken, getAccessTokenCookieOptions()).cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
+
+export const clearAuthCookies = (res: Response) =>
+    res.clearCookie("accessToken").clearCookie("refreshToken", { path:REFRESH_PATH});
