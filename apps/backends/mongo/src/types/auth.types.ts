7fe4a3f9381e7420196511b05bb213faf5e1aef6
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-20 22:28:10 +0530

    Implement jwt reusable signToken and verifyToken functionality

---
+++ apps/backends/mongo/src/types/auth.types.ts
@@ -0,0 +1,11 @@
+import { SessionDocument } from "./session.types";
+import { UserDocument } from "./user.types";
+
+export type RefreshTokenPayload = {
+  sessionId: SessionDocument["_id"];
+};
+
+export type AccessTokenPayload = {
+  userId: UserDocument["_id"];
+  sessionId: SessionDocument["_id"];
+};
---
+++ apps/backends/mongo/src/utils/jwt.ts
@@ -0,0 +1,56 @@
+import { SignOptions, VerifyOptions } from "jsonwebtoken"
+import { SessionDocument } from "../types/session.types"
+import { UserDocument } from "../types/user.types"
+import jwt from "jsonwebtoken";
+import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
+import { AccessTokenPayload, RefreshTokenPayload } from "../types/auth.types";
+
+
+
+type SignOptionsAndSecret = SignOptions & {
+    secret: string
+}
+
+const accessTokenSignOptions: SignOptionsAndSecret = {
+    expiresIn: "15m",
+    secret: JWT_SECRET
+}
+export const refreshTokenSignOptions: SignOptionsAndSecret = {
+    expiresIn: "30d",
+    secret: JWT_REFRESH_SECRET
+}
+
+export const singToken = (
+    payload: AccessTokenPayload | RefreshTokenPayload,
+    options?: SignOptionsAndSecret
+) => {
+    const { secret, ...signOpts } = options || accessTokenSignOptions
+    return jwt.sign(payload, secret, signOpts);
+}
+
+export const verifyToken = <TPayload extends object = AccessTokenPayload>(
+    token: string,
+    options?: VerifyOptions & { secret: string }
+) => {
+    const { secret = JWT_SECRET, ...verifyOpts } = options || {};
+    try {
+        const payload = jwt.verify(
+            token,
+            secret,
+            { ...verifyOpts }
+        ) as TPayload;
+        return {
+            payload
+        }
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                error: error.message
+            }
+        }
+
+        return {
+            error: "Unknown error occured please try again!"
+        };
+    }
+}
