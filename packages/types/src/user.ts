8d48ee296089e336693a015db13245fd30b061a1
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-24 22:45:07 +0530

    Update the zod types with new constants

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -14,10 +14,12 @@ export const credentialsDataSchema = credentialSchema.extend({
 export type CredentialSchema = z.infer<typeof credentialsDataSchema>;
 
 // register inputs
-export const registerSchema = credentialSchema.extend({
+const registerFieldsSchema = credentialSchema.extend({
     name: z.string().min(1).max(255),
     confirmPassword: z.string().min(6).max(255),
-}).refine(
+})
+
+export const registerSchema = registerFieldsSchema.refine(
     (data) => data.password === data.confirmPassword, {
     message: "Password do not match",
     path: ["confirmPassword"]
@@ -29,3 +31,22 @@ export type RegisterSchema = z.infer<typeof registerSchema>;
 // login inputs
 export const loginInSchema = credentialSchema;
 export type LoginInSchema = z.infer<typeof loginInSchema>;
+
+
+// email input for forgot password
+export const emailSchema = credentialSchema.pick({ email: true });
+export type EmailSchema = z.infer<typeof emailSchema>
+
+
+// password validation
+export const resetPasswordSchema = registerFieldsSchema.pick({
+    password: true,
+    confirmPassword: true
+}).refine(
+    (data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"]
+})
+
+
+export type ResetPasswordSchema = z.infer<typeof resetPasswordSchema>;
