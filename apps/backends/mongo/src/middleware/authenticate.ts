017e85108dceabb57cdc0517a3f96f5858282a77
venkateshg12 <gvenkatesh1204@gmail.com>
2026-06-24 22:44:35 +0530

    Implement authenticate middleware

---
+++ apps/backends/mongo/src/middleware/authenticate.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from "express";
+import appAssert from "../utils/appAssert";
+import { UNAUTHORIZED } from "../constants/https";
+import { appErrorCode } from "../constants/appErrorCode";
+import { verifyToken } from "../utils/jwt";
+
+const authenticate: RequestHandler = (req, res, next) => {
+    const accessToken = req.cookies.accessToken as string | undefined;
+    appAssert(accessToken, UNAUTHORIZED, "Not Authorized", appErrorCode.InvalidAccessToken);
+
+    const { error, payload } = verifyToken(accessToken);
+
+    appAssert(payload, UNAUTHORIZED, error == "jwt expired" ? "Token expired" : "Invalid token", appErrorCode.InvalidAccessToken);
+
+    req.userId = payload.userId;
+    req.sessionId = payload.sessionId;
+    next();
+
+}
