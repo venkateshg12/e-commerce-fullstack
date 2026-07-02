e837ce2057b4d30faac9cd333654593cdc28e533
venkateshg12 <gvenkatesh1204@gmail.com>
2026-07-02 22:50:19 +0530

    commit

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+  resolve: {
+    alias: {
+      "@": path .resolve(__dirname, "./src"),
+    },
+  },
 });
