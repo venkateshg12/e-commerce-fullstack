8a0e68ce6dac4baf089db59fd7c1ff8b39d67a4e
venkateshg12 <gvenkatesh1204@gmail.com>
2026-09-20 22:29:45 +0530

    feat(rate-limit): implement sliding-window lua scripts and circuit breaker

--- apps/backends/mongo/src/types/express.d.ts
+++ apps/backends/mongo/src/types/express.d.ts
@@ -1,11 +1,15 @@
-
 import { Types } from "mongoose";
 import { UserDocument } from "./user.types";
 
-declare module "express-serve-static-core" {
-    interface Request {
-        userId: Types.ObjectId;
-        sessionId: Types.ObjectId;
-        role : UserDocument["role"];
+declare global {
+    namespace Express {
+        interface Request {
+            userId: Types.ObjectId;
+            sessionId: Types.ObjectId;
+            role: UserDocument["role"];
+            visitorId?: string;
+        }
     }
 }
+
+export {};
